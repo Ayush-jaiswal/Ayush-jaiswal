b5fb88fa0b7fe5fb495951308578301ee48a56de
Ayush-jaiswal <ayushjaiswalaj@gmail.com>
2018-07-31 22:46:00 +0530

    part1

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+
+$(".Dropdown a").click(function(){
+	$(".Dmenu").slideToggle();
+
+});
+
+
+
+$("#drawerbutton").click(function(){
+	
+$("#disappear1").slideToggle(300,function(){
+
+});
+
+});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
